9c7ac46d8a8ec9e9d97d661b1140a404a84baa94
Diego Miranda <diego.miranda.h@gmail.com>
2020-11-30 02:31:33 -0300

    fix daemonrecord

--- daemonRecord.py
+++ daemonRecord.py
@@ -47,8 +47,7 @@ if __name__ == '__main__':
 		else:
 			p = subprocess.Popen([sys.executable, ProcessFileName, "child", args], stdout = PIPE, stderr = PIPE)
 			pid = p.pid
-			if not os.path.exists(pidFile):
-				os.mknod(pidFile)
+			os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 		print("Service", pidname, pid, "started", flush=True)
 		# create processfile to signify process has started
